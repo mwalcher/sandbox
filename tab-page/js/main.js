214e78ab19119ed3125d7c6d7b386e4766004fed
Matt Walcher <matt@mwalcher.com>
2021-02-23 23:30:54 -0500

    updated script to work for on page load if hash is present

--- tab-page/js/main.js
+++ tab-page/js/main.js
@@ -1,15 +1,34 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const tabSections = Array.from(document.querySelectorAll('section[id]'));
     const tabNavigation = Array.from(document.querySelectorAll('header nav a[href*="#"]'));
+    const activeClass = 'active';
 
-    function toggleSection(e) {
-        const selectedLink = e.target;
-        const hash = selectedLink.href.substring(selectedLink.href.indexOf('#') + 1);
-        const selectedSection = tabSections.find(section => section.id === hash);
-        console.log(selectedSection);
+    function toggleSection(hash) {
+        const sectionID = hash.substring(1);
+        const selectedLink = tabNavigation.find(link => link.href === hash);
+        const selectedSection = tabSections.find(section => section.id === sectionID);
+
+        if (selectedLink) {
+            selectedLink.classList.add(activeClass);
+        }
+
+        if (selectedSection) {
+            selectedSection.classList.add(activeClass);
+        }
     }
 
     tabNavigation.forEach(navLink => {
-        navLink.addEventListener('click', toggleSection);
+        navLink.addEventListener('click', (e) => {
+            const selectedLink = e.target;
+            const hash = selectedLink.href.substring(selectedLink.href.indexOf('#'));
+            toggleSection(hash);
+        });
     });
+
+    window.addEventListener('load', () => {
+        const { hash } = window.location;
+        if (hash) {
+            toggleSection(hash);
+        }
+    })
 });
