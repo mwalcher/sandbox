9a2c4fcf65e078321387082cafef617b206c9c0a
Matthew Walcher <matt@mwalcher.com>
2023-02-21 22:19:25 +0000

    Set up base storybook with sass modules (#1)
    * add base vite project
    
    * add base storybook project root
    
    * add base storybook files
    
    * rebuild project in node 16
    
    * add base storybook
    
    * add new addons
    
    * update button to use css modules
    
    * update vue components to use sass modules
    
    * update button to use sass modules

---
+++ storybook/.storybook/main.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+    '@storybook/preset-scss',
+  ],
+  framework: '@storybook/vue3',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+}
