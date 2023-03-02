7d85020766ee7e897d0b4e8c10fadc6e13b3572d
Matthew Walcher <matt@mwalcher.com>
2023-03-02 15:08:52 +0000

    Remove default components and set up base component files

---
+++ pinia-intro/src/components/TodoApp.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import TodoForm from './TodoForm.vue';
+import TodoList from './TodoList.vue';
+</script>
+
+<template>
+  <TodoForm />
+  <TodoList />
+</template>
---
+++ pinia-intro/src/components/TodoList.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup>
+//
+</script>
+
+<template>TODO LIST (child)</template>
