5a4d17606c7f1891a68f17ae5e8d70dd23d3a4ee
Matthew Walcher <matt@mwalcher.com>
2023-03-02 16:12:37 +0000

    add base list

--- pinia-intro/src/components/TodoList.vue
+++ pinia-intro/src/components/TodoList.vue
@@ -1,5 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia';
+import { useTodoListStore } from '@/stores/todoList';
+
+const store = useTodoListStore();
+const { todoList } = storeToRefs(store);
+</script>
 
 <template>
-  <div></div>
+  <div v-for="todo in todoList" :key="todo.id" class="item">
+    <div class="content">
+      <span>{{ todo.item }}</span>
+    </div>
+  </div>
 </template>
