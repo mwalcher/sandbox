b735caa16f5b2e8707907b0761261dbe481badd7
Matthew Walcher <matt@mwalcher.com>
2025-03-21 22:46:44 +0000

    test dynamic asset

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+function getImageUrl(name) {
+  // note that this does not include files in subdirectories
+  return new URL(`./assets/subdir/${name}.svg`, import.meta.url).href
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+    <img alt="Vue logo" class="logo" :src="getImageUrl('logo')" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
